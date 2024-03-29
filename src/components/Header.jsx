791ba1c36534ec64b26979d49484f727d73960f0
mustafapeksen <mustafapeksen04@gmail.com>
2024-03-29 14:20:45 +0300

    feat: Added logo and icon
    Added logo and icon to the project for better visual representation.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,11 @@ import React from "react";
 function Header() {
   return (
     <div id="heading">
+      <img
+        id="logo"
+        src="/image/SeyyahlarLogo.jpg"
+        alt="logo"
+      />
       <h1>Seyyahlar Haber</h1>
       <hr />
     </div>
